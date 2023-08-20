f7a0eff1d13b515b680ab88c03bd0b016582efdb
nick <>
2023-08-19 21:45:42 -0400

    * saving settings

--- CallsignToolkit/Exceptions/CallsignNotFound.cs
+++ CallsignToolkit/Exceptions/CallsignNotFound.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CallsignToolkit.Exceptions
+﻿namespace CallsignToolkit.Exceptions
 {
     public class CallsignNotFound : Exception
     {
